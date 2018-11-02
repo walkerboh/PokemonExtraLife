7bcfa494ce612ede475b8eab61d57c46c9ee7c65
Evan Frisch <walkerboh1213@gmail.com>
2018-11-01 20:03:07 -0400

    Rounding pokemon health with multiplier

--- PokemonExtraLifeApi/Models/API/Pokemon.cs
+++ PokemonExtraLifeApi/Models/API/Pokemon.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 
 namespace PokemonExtraLifeApi.Models.API
 {
@@ -10,7 +11,7 @@ namespace PokemonExtraLifeApi.Models.API
         public decimal StartingHealth { get; set; }
         public decimal Damage { get; set; }
         public decimal HealthMultiplier { get; set; } = 1;
-        public decimal TotalHealth => StartingHealth * HealthMultiplier;
+        public decimal TotalHealth => Math.Floor(StartingHealth * HealthMultiplier);
         public decimal CurrentHealth => TotalHealth - Damage;
 
         [JsonIgnore]
